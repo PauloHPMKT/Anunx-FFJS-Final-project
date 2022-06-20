20794206acc80d44bd28d1c73cd711afef689352
Paulo Sergio <pauloserg861@gmail.com>
2022-06-19 21:55:13 -0300

    estrutura inicial de projeto

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000000',
+        }
+    }
+})
+
+export default theme
