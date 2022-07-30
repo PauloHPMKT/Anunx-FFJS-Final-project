71dfb63d0a84d6d28322527314ea384baec6293e
Paulo Sergio <pauloserg861@gmail.com>
2022-07-29 22:15:43 -0300

    criando tema secundario

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,9 @@ const theme = createTheme({
         primary: {
             main: '#000000',
         },
+        secondary: {
+            main: '#fff',
+        },
         background: {
             default: 'rgb(242, 244, 245)',
             white: '#ffffff',
