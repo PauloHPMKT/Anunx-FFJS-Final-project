414473d094df221fdda95881c9452e1e5855813c
Paulo Sergio <pauloserg861@gmail.com>
2022-08-04 21:52:27 -0300

    header - adicionando container e menu operacional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,80 @@
 import * as React from 'react';
+import { useState } from 'react'
+import Link from 'next/link';
 import { 
   AppBar,
-  Box,
   Toolbar,
   Typography,
   Button,
   IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Divider,
 } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu';
+import { Container } from '@mui/system';
+import { AccountCircle } from '@mui/icons-material';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  userName: {
+    marginLeft: '6px'
+  },
+  divider: {
+    margin: '8px 0'
+  }
+})
 
 export default function ButtonAppBar() {
+  const classes = useStyles()
+
+  const [anchorUserMenu, setAnchorUseMenu] = useState(false)
+
+  const openUserMenu = Boolean(anchorUserMenu)
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <AppBar position="static" elevation={3}>
+      <Container maxWidth="lg">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Anunx
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link href="/user/publish" passHref>
+            <Button color="inherit" variant="outlined">
+              Anunciar e Vender
+            </Button>
+          </Link>
+          <IconButton color="secondary" onClick={(e) => setAnchorUseMenu(e.currentTarget)}>
+            {
+              true === false
+                ? <Avatar src="" />
+                : <AccountCircle />
+            }
+            <Typography variant="subtitle2" color="secondary" className={classes.userName}>
+              Paulo Sérgio
+            </Typography>
+          </IconButton>
+          
+          <Menu
+            anchorEl={anchorUserMenu}
+            open={openUserMenu}
+            onClose={() => setAnchorUseMenu(null)}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <Link href="/user/dashboard" passHref>
+              <MenuItem>Meus anúncios</MenuItem>
+            </Link>
+            <Link href="/user/publish" passHref> 
+              <MenuItem>Publicar novo anúncio</MenuItem>
+            </Link>
+            <Divider className={classes.divider}/>
+            <MenuItem>Sair</MenuItem>
+          </Menu>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 }
